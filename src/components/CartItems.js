4e72c04b7b78113ee0134d223e47c8e0f6b9aeb2
163002029.junayed <junayednoor@gmail.com>
2022-07-01 20:36:45 +0600

    Cart-slice

--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -1,14 +1,19 @@
-import React from "react";
-import CartItem from "./CartItem";
-import "./Cart.css";
+import React from 'react';
+
+import './Cart.css';
+import { useSelector } from 'react-redux';
+import CartItem from './CartItem';
 const CartItems = () => {
+  const cartItems = useSelector((state) => state.cart.itemsList);
   return (
     <div className="cart-container">
       <h2>Your Cart</h2>
       <ul>
-        <li>
-          <CartItem id={1} price={2500} name={"Macbook"} />
-        </li>
+        {cartItems.map((item) => (
+          <li key={item.id}>
+            <CartItem item={item}></CartItem>
+          </li>
+        ))}
       </ul>
     </div>
   );
