3e5a9c4a4e2d287b3664fda1d9e46b4c8a3cab33
163002029.junayed <junayednoor@gmail.com>
2022-07-04 15:51:15 +0600

    Firebase Realetime database

--- src/App.js
+++ src/App.js
@@ -3,10 +3,23 @@ import './App.css';
 import Auth from './components/Auth';
 import { useSelector } from 'react-redux';
 import Layout from './components/Layout';
+import { useEffect } from 'react';
 
 function App() {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-  const cartItems = useSelector((state) => state.cart.itemsList);
+  const cart = useSelector((state) => state.cart);
+  useEffect(() => {
+    const sendRequest = async () => {
+      const res = await fetch(
+        'https://react-redux-http-85a7c-default-rtdb.firebaseio.com/cartItems.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart),
+        }
+      );
+      const data = await res.json();
+    };
+  }, [cart]);
   return (
     <div className="App">
       {!isLoggedIn && <Auth />}
